9b36704a59a99b1b83dfb540165197c4c2d9b3be
ammatsui <54817160+ammatsui@users.noreply.github.com>
2020-12-25 12:31:03 +0200

    Delete lev.hs

--- crpt/lev.hs
+++
@@ -1,4 +0,0 @@
-lev :: String -> String -> Int
-lev []     b      = length b
-lev a      []     = length a
-lev (a:as) (b:bs) = if (a == b) then (lev as bs) else 1 + min $ (lev as (b:bs)) (lev (a:as) bs) (lev as bs)
